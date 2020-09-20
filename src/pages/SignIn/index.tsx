af6c0516caeb51702336740f675b6cf5e1f4564c
Abilton Dias <abiltongd@gmail.com>
2020-09-20 00:38:33 -0300

    Implementado o Unform nos formulários do projeto

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Image, View, ScrollView ,KeyboardAvoidingView, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -10,12 +13,19 @@ import {
   Container,
   Title,
   ForgoutPasswordButton,
-  ForgoutPasswordText
+  ForgoutPasswordText,
   CreateAccountButton,
   CreateAccountText
 } from './styles';
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation();
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, [])
+
   return(
     <>
       <KeyboardAvoidingView
@@ -33,20 +43,21 @@ const SignIn: React.FC = () => {
           <View>
             <Title> Faça o seu logon</Title>
           </View>
-          <Input name='email' icon='mail' placeholder='E-mail' />
-          <Input name='password' icon='lock' placeholder='Senha'/>
+          <Form ref={formRef} onSubmit={handleSignIn}>
+            <Input name='email' icon='mail' placeholder='E-mail' />
+            <Input name='password' icon='lock' placeholder='Senha'/>
 
-          <Button onPress={() => {
-            console.log('foi!')
-          }} > Entrar </Button>
-
+            <Button onPress={() => {
+              formRef.current?.submitForm
+            }} > Entrar </Button>
+          </Form>
           <ForgoutPasswordButton onPress={() => {}}>
             <ForgoutPasswordText> Esqueci minha Senha </ForgoutPasswordText>
           </ForgoutPasswordButton>
         </Container>
       </ScrollView>
         </KeyboardAvoidingView>
-      <CreateAccountButton onPress={() => {}}>
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
         <Icon name="log-in" size={20} color="#ff9000" />
         <CreateAccountText>Criar uma conta</CreateAccountText>
       </CreateAccountButton>
