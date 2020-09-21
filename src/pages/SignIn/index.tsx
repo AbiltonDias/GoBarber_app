25b5b2d7ecefbd31cc6faac23b58ebe4b65cc948
Abilton Dias <abiltongd@gmail.com>
2020-09-21 05:57:44 -0300

    Incluindo verificador dos Input e fazer UX para usabilidade do app

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useRef } from 'react';
-import { Image, View, ScrollView ,KeyboardAvoidingView, Platform } from 'react-native';
+import { Image, View, ScrollView ,KeyboardAvoidingView, Platform, TextInput } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
@@ -21,6 +21,7 @@ import {
 const SignIn: React.FC = () => {
   const navigation = useNavigation();
   const formRef = useRef<FormHandles>(null);
+  const passwordInputRef = useRef<TextInput>(null);
 
   const handleSignIn = useCallback((data: object) => {
     console.log(data);
@@ -44,11 +45,34 @@ const SignIn: React.FC = () => {
             <Title> Faça o seu logon</Title>
           </View>
           <Form ref={formRef} onSubmit={handleSignIn}>
-            <Input name='email' icon='mail' placeholder='E-mail' />
-            <Input name='password' icon='lock' placeholder='Senha'/>
+            <Input
+              autoCorrect={false}
+              autoCapitalize='none'
+              keyboardType='email-address'
+              name='email'
+              icon='mail'
+              placeholder='E-mail'
+              returnKeyType='next'
+              onSubmitEditing={() => {
+                passwordInputRef.current?.focus();
+              }}
+
+            />
+
+            <Input
+              ref={passwordInputRef}
+              name='password'
+              icon='lock'
+              placeholder='Senha'
+              secureTextEntry
+              returnKeyType='send'
+              onSubmitEditing={() => {
+                formRef.current?.submitForm();
+              }}
+            />
 
             <Button onPress={() => {
-              formRef.current?.submitForm
+              formRef.current?.submitForm();
             }} > Entrar </Button>
           </Form>
           <ForgoutPasswordButton onPress={() => {}}>
