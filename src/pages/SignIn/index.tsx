5bbccb584ac56d9fbadbf2120dc8852e5132b177
Abilton Dias <abiltongd@gmail.com>
2020-10-02 14:32:19 -0300

    Implementado funcionalidades de verificação de erros

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,20 @@
 import React, { useCallback, useRef } from 'react';
-import { Image, View, ScrollView ,KeyboardAvoidingView, Platform, TextInput } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView ,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 
+import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
@@ -18,14 +28,53 @@ import {
   CreateAccountText
 } from './styles';
 
+interface SignInFormData{
+  email: string;
+  password:string;
+}
+
 const SignIn: React.FC = () => {
   const navigation = useNavigation();
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
 
-  const handleSignIn = useCallback((data: object) => {
-    console.log(data);
-  }, [])
+  const handleSignIn = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('O email é obrigatorio')
+            .email('Digite um email válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+        //await signIn({
+        //  email: data.email,
+        //  password: data.password,
+        //});
+
+        //history.push('/dashboard');
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        Alert.alert(
+          'Error na Autenticação',
+          'Ocorreu um erro ao fazer login, cheque as credenciais!'
+        )
+      }
+    },
+    []
+  );
 
   return(
     <>
--- src/pages/SignUp/Index.tsx
+++ src/pages/SignUp/Index.tsx
@@ -1,17 +1,20 @@
-import React, { useRef } from 'react';
+import React, { useRef, useCallback } from 'react';
 import {
   Image,
   View,
   ScrollView,
   KeyboardAvoidingView,
   Platform,
-  TextInput
+  TextInput,
+  Alert
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 
+import getValidationErrors from '../../utils/getValidationErrors';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import logoImg from '../../assets/logo.png';
@@ -23,12 +26,60 @@ import {
   BackToSignInText
 } from './styles';
 
+interface SignUpFormData{
+  name: string;
+  email: string;
+  password:string;
+}
+
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
 
   const emailInputRef = useRef<TextInput>(null);
   const passwordInputRef = useRef<TextInput>(null);
+
+  const handleSignUp = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome é obrigatório'),
+          email: Yup.string()
+            .required('O email é obrigatorio')
+            .email('Digite um email válido'),
+          password: Yup.string().min(6, 'No mínimo 6 digitos'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        //await api.post('/users', data);
+
+        //history.push('/');
+
+        Alert.alert(
+          'Cadastro realizado!',
+          'Você já pode fazer seu logon no GoBarber.'
+        )
+
+      } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(error);
+          formRef.current?.setErrors(errors);
+          return;
+        }
+
+        Alert.alert(
+          'Erro no Cadastro!',
+          'Ocorreu erro ao fazer o cadastro, tente novamente.'
+        );
+      }
+    },
+    [],
+  );
 
   return(
     <>
@@ -47,9 +98,7 @@ const SignUp: React.FC = () => {
           <View>
             <Title> Crie sua conta</Title>
           </View>
-          <Form ref={formRef} onSubmit={(data) => {
-            console.log(data);
-          }}>
+          <Form ref={formRef} onSubmit={handleSignUp}>
             <Input
               name='name'
               icon='user'
