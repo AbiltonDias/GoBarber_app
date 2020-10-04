84df1485cab6c0888fa6e2e3c28591c4e8f69f5f
Abilton Dias <abiltongd@gmail.com>
2020-10-03 23:20:48 -0300

    implementado routes do app

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -22,6 +22,7 @@ interface SignInCredentials{
 
 interface AuthContextData{
     user:object;
+    loading: boolean;
     signIn(credentials: SignInCredentials): Promise<void>;
     signOut():void;
 }
@@ -31,6 +32,7 @@ const AuthContext = createContext<AuthContextData>(
 
 const AuthProvider: React.FC =  ({children}) => {
     const [data, setData] = useState<AuthState>({} as AuthState);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
       async function loadStorageData(): Promise<void>{
@@ -44,7 +46,10 @@ const AuthProvider: React.FC =  ({children}) => {
         if(token[1] && user[1]){
           setData({token: token[1], user: JSON.parse(user[1])});
         }
+
+        setLoading(false);
       }
+      loadStorageData();
     }, [])
 
     const signIn = useCallback(async ({email, password}) => {
@@ -77,7 +82,7 @@ const AuthProvider: React.FC =  ({children}) => {
     }, []);
 
     return(
-        <AuthContext.Provider value={{user: data.user, signIn, signOut}}>
+        <AuthContext.Provider value={{user: data.user, loading,signIn, signOut}}>
             {children}
         </AuthContext.Provider>
     );
